4aabee7d0f075ce529f2b4835f763abc32052bef
maheekasun4-star <maheekasun8@gmail.com>
2026-09-08 12:03:10 +0530

    test

--- shared-theme/tailwind-preset.js
+++ shared-theme/tailwind-preset.js
@@ -2,7 +2,7 @@ const preset = {
   theme: {
     colors: {
       slate: {
-        50: '#f8fafc',
+        50: '#f8fafc', 
         100: '#f1f5f9',
         200: '#e2e8f0',
         300: '#cbd5e1',
